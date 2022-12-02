f51a5c6acb3f084e53635151857b7667b3bbaa20
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2022-12-02 20:13:29 +0300

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -6,56 +6,81 @@ fn main() {
     cwd.push("inputs.txt");
     let text_inputs = read_to_string(cwd.into_os_string()).expect("Error when reading file");
 
+    // ------------ Game rules
+    let me=1;
+    let opponent = 0;
+
     // ----------- Turn input into array
-    let input_array = text_inputs.split("\n\n");
-    let mut elves_foods=Vec::new();
-    for input in input_array {
-        let mut elve_foods=Vec::new();
-        for food in input.split("\n") {
-            elve_foods.push(food.parse::<i32>().unwrap());
-        }
-        elves_foods.push(elve_foods);
-    };
+    let game_rounds = text_inputs.split("\n").map(|round| {
+        let round_splited = round.split(" ");
+        round_splited.collect::<Vec<&str>>()
+    }).collect::<Vec<Vec<&str>>>();
 
-    // ------------ Reduce to list of numbers
-    let mut elves_calories=Vec::new();
-    for foods in elves_foods {
-        let mut sum=0;
-        for food in foods {
-            sum = sum + food
-        }
-        elves_calories.push(sum)
+    // ------------- Calculate all game scores
+    let games_scores = game_rounds.iter().map(|game| {
+        let game_shape_points = (shape_to_point(game[opponent]),shape_to_point(game[me]));
+        let game_match_points=shape_match(game[opponent], game[me]);
+        (game_shape_points.0+game_match_points.0,game_shape_points.1+game_match_points.1)
+    }).collect::<Vec<(i32,i32)>>();
+
+    // -------------- Get my scores
+    let my_scores=games_scores.iter().map(|game| game.1).collect::<Vec<i32>>();
+
+    // -------------- Get my total points
+    let my_points:i32 = my_scores.iter().sum();
+    println!("Step1: {}",my_points);
+
+    // ------------ Calculate step 2 game score
+    let new_games_scores = game_rounds.iter().map(|game| {
+        let my_move = shape_to_move(game[opponent], game[me]);
+        let game_shape_points = (shape_to_point(game[opponent]),shape_to_point(my_move));
+        let game_match_points=shape_match(game[opponent], my_move);
+        (game_shape_points.0+game_match_points.0,game_shape_points.1+game_match_points.1)
+    }).collect::<Vec<(i32,i32)>>();
+
+    // -------------- Get my scores for step 2
+    let my_new_scores=new_games_scores.iter().map(|game| game.1).collect::<Vec<i32>>();
+
+    // -------------- Get my total points
+    let my_new_points:i32 = my_new_scores.iter().sum();
+    println!("Step2: {}",my_new_points);
+}
+
+fn shape_to_point(shape:&str)->i32{
+    match shape {
+        "A" | "X"=>1,
+        "B" | "Y"=>2,
+        "C" | "Z"=>3,
+        _=>panic!("Wrong shape")
     }
-    println!("List: {:?}",elves_calories);
+}
 
-    // ------------ Find the maximum
-    let mut max_elf=0;
-    let mut i=0;
-    let mut j=elves_calories.len() / 2;
-    loop {
-        max_elf = if elves_calories[i] > max_elf {
-            elves_calories[i]
-        } else if elves_calories[j] > max_elf {
-            elves_calories[j]
-        } else {
-            max_elf
-        };
-        if j<elves_calories.len() {
-            j = j + 1
-        }
-        if i<(elves_calories.len() / 2)+1 {
-            i = i + 1
-        }
-        if i>=elves_calories.len() / 2 && j>=elves_calories.len() {
-            break;
-        }
+fn shape_match(op:&str,me:&str)->(i32,i32) {
+    match (op,me) {
+        ("A","X")=>(3,3),
+        ("A","Y")=>(0,6),
+        ("A","Z")=>(6,0),
+        ("B","X")=>(6,0),
+        ("B","Y")=>(3,3),
+        ("B","Z")=>(0,6),
+        ("C","X")=>(0,6),
+        ("C","Y")=>(6,0),
+        ("C","Z")=>(3,3),
+        _=>panic!("Wrong game, OP:{}, ME:{}",op,me)
     }
+}
 
-    // --------------- Finding the top 3
-    elves_calories.sort();
-    let top_3:&i32 = &elves_calories[elves_calories.len()-3..].iter().sum();
-
-    // ---------------- Print the result
-    println!("Max: {}",max_elf);
-    println!("Top3: {}",top_3);
+fn shape_to_move<'a>(op:&str,me:&str)->&'a str{
+    match (op,me) {
+        ("A","X")=>"Z",
+        ("B","X")=>"X",
+        ("C","X")=>"Y",
+        ("A","Y")=>"X",
+        ("B","Y")=>"Y",
+        ("C","Y")=>"Z",
+        ("A","Z")=>"Y",
+        ("B","Z")=>"Z",
+        ("C","Z")=>"X",
+        _=>panic!("Wrong game II, OP:{}, ME:{}",op,me)
+    }
 }
