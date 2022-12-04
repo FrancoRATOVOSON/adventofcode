cb748af19b020e6ce85de52b81dc69f67b5cee30
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2022-12-04 21:45:02 +0300

    Day 4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::{env::current_dir, fs::read_to_string};
 
+type SectionRange = Vec<i32>;
+type AssignementGroup = Vec<SectionRange>;
+
 fn main() {
     // ----------- Get the inputs
     let mut cwd = current_dir().expect("Error when finding current working directory");
@@ -7,59 +10,51 @@ fn main() {
     let text_inputs = read_to_string(cwd.into_os_string()).expect("Error when reading file");
 
     // ----------- Turn input into array
-    let mut rucksacks = text_inputs.split("\n").map(|rucksack| {
-        let rucksack_content = rucksack.split_at(rucksack.len()/2);
-        (String::from(rucksack),common_letter(rucksack_content))
-    }).collect::<Vec<(String,char)>>();
+    let section_pairs = text_inputs.split("\n").map(|pair| {
+        let assignements = pair.split(",");
+        assignements.map(|section| {
+            section.split("-").map(|number| number.parse::<i32>().unwrap()).collect::<SectionRange>()
+        }).collect::<AssignementGroup>()
+    }).collect::<Vec<AssignementGroup>>();
 
-    // ------------- Calculate all priorites
-    let priorities_list = rucksacks.iter().map(|rucksack| {
-        get_priority(rucksack.1)
-    }).collect::<Vec<i32>>();
+    // ------------- Get all assignement pair
+    let assignement_pair = section_pairs.iter().map(|pair|{
+        is_contained(pair)
+    }).collect::<Vec<bool>>();
 
-    // -------------- Get prorities sum
-    let priorities:i32 = priorities_list.iter().sum();
-    println!("Step1: {}",priorities);
+    // ------------- Get all assignement pair with one range fully contain the other
+    let assignement_with_range = assignement_pair.iter().filter(|pair| {
+        **pair == true
+    }).collect::<Vec<&bool>>();
 
-    // -------------- Group rucksacks by 3
-    let mut grouped_rucksacks:Vec<Vec<(String,char)>>=Vec::new();
-    loop {
-        grouped_rucksacks.push(rucksacks.drain(..3).collect::<Vec<(String,char)>>());
-        if rucksacks.len() <= 0 {
-            break;
-        }
-    }
+    // println!("With range: {:?}",assignement_pair);
+    println!("Step1: {}",assignement_with_range.len());
 
-    // -------------- Calculate all priorites
-    let priorities_list = grouped_rucksacks.iter().map(|rucksacks| {
-        let priority_letter = common_item(rucksacks);
-        get_priority(priority_letter)
-    }).collect::<Vec<i32>>();
-
-    // -------------- Get prorities sum
-    let priorities:i32 = priorities_list.iter().sum();
-    println!("Step1: {}",priorities);
+    // ------------- Get all assignement pair with ranges overlapping
+    let assignement_overlaps = section_pairs.iter().map(|pair|{
+        is_overlapping(pair)
+    }).filter(|pair| {
+        *pair == true
+    }).collect::<Vec<bool>>();
+    
+    // println!("With range: {:?}",assignement_pair);
+    println!("Step1: {}",assignement_overlaps.len());
 }
 
-fn common_letter(chars:(&str,&str))->char {
-    for chr_1 in chars.0.chars().enumerate() {
-        if chars.1.contains(chr_1.1) {
-            return chr_1.1
-        }
-    }
-    panic!("No Match on items");
+fn is_contained(group:&AssignementGroup)->bool {
+    let assignement_1 = &group[0];
+    let assignement_2 = &group[1];
+    let assignement2_in_assignement1 = assignement_2[0] >= assignement_1[0] && assignement_2[1] <= assignement_1[1];
+    let assignement1_in_assignement2 = assignement_1[0] >= assignement_2[0] && assignement_1[1] <= assignement_2[1];
+    assignement1_in_assignement2 || assignement2_in_assignement1
 }
 
-fn common_item(rucksacks:&Vec<(String,char)>)->char {
-    for chr in rucksacks[0].0.chars().enumerate() {
-        if rucksacks[1].0.contains(chr.1) && rucksacks[2].0.contains(chr.1) {
-            return  chr.1;
-        }
-    }
-    panic!("No common items");
-}
-
-fn get_priority(item:char)->i32{
-    let priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    (priorities.find(item).expect("Priority error ") + 1) as i32
+fn is_overlapping(group:&AssignementGroup)->bool {
+    let assignement_1 = &group[0];
+    let range_1 = assignement_1[0]..assignement_1[1]+1;
+    let assignement_2 = &group[1];
+    let range_2 = assignement_2[0]..assignement_2[1]+1;
+    let assignement2_in_assignement1 = range_1.contains(&assignement_2[0]) ||range_1.contains(&assignement_2[1]);
+    let assignement1_in_assignement2 = range_2.contains(&assignement_1[0]) ||range_2.contains(&assignement_1[1]);
+    assignement2_in_assignement1 || assignement1_in_assignement2
 }
